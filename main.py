947bf93608d4fb7a23e132bed95679dff1126407
IsolatedSushi <s.vanherpt@students.uu.nl>
2021-09-24 13:35:42 +0200

    Added gini index

--- main.py
+++ main.py
@@ -3,11 +3,51 @@ import pandas as pd
 
 credit_data = np.genfromtxt('credit.txt', delimiter=',', skip_header=True)
 
-print(credit_data)
+data = credit_data[:, :5] 
+classLabels = credit_data[:, 5]
+
+splitGroup1 = data[:5,:]
+splitGroup2 = data[5:10,:]
+
+splitLabels1 = classLabels[:5]
+splitLabels2 = classLabels[5:10]
+
+
 
 
 def tree_grow(x, y, nmin: int, minleaf: int, nfeat):
+
+
+
     return None
+
+def get_best_split(data, labels):
+    income_sorted = np.sort(np.unique(data[:,3]))
+
+
+
+
+    # for featureIndex in range(len(data)):
+    #     for row in data:
+    #         leftDataSet, rightDataSet, leftLabelSet, rightLabelSet = get_current_split(featureIndex, row[featureIndex], data, labels)
+    #         gineValue = gini_index
+
+    return None
+
+#TODO use the numpy indexing for this instead of the for loop
+def get_current_split(featureIndex, featureSplitValue, data, labels):
+    leftDataSet, rightDataSet = [], []
+    leftLabelSet, rightLabelSet = [], []
+
+    for index, dataRow in enumerate(data):
+        if data[featureIndex] < featureSplitValue:
+            leftDataSet.append(dataRow)
+            leftLabelSet.append(labels[index])
+        else:
+            rightDataSet.append(dataRow)
+            rightLabelSet.append(labels[index])
+
+    return leftDataSet, rightDataSet, leftLabelSet, rightLabelSet
 
 
 def tree_pred(x, tree):
@@ -21,24 +61,28 @@ def tree_grow_b():
 def tree_pred_b():
     return None
 
+# Calculates the gini_index using the formulate from slide ....  
+def gini_index(labels):
+    totalTrue = np.sum(labels)
+    totalAmount = labels.shape[0]
 
-# --------------------------------------------
-# Calculate the Gini index for a split dataset
-def gini_index(groups, classes):
-    # count all samples at split point
-    n_instances = float(sum([len(group) for group in groups]))
-    # sum weighted Gini index for each group
-    gini = 0.0
-    for group in groups:
-        size = float(len(group))
-        # avoid divide by zero
-        if size == 0:
-            continue
-        score = 0.0
-        # score the group based on the score for each class
-        for class_val in classes:
-            p = [row[-1] for row in group].count(class_val) / size
-            score += p * p
-        # weight the group score by its relative size
-        gini += (1.0 - score) * (size / n_instances)
-    return gini
+    probTrue = totalTrue / totalAmount
+
+    return probTrue * (1 - probTrue)
+
+
+print(gini_index(np.array([1,0,1,1,1,0,0,1,1,0,1])))
+
+
+
+# TODO
+# Make a tree structure
+# Draw Tree
+# Get Possible splits
+# Evaluate splits
+# Make split
+# Form tree
+# Make predictions
+# Finalise
+# Bagging
+# Random Forests
---
+++ tree.py
@@ -0,0 +1,10 @@
+
+# class Tree:
+#     def __init__(self):
+#         pass
+
+
+class Node:
+    def __init__(self):
+        self.left = None
+        self.right = None
