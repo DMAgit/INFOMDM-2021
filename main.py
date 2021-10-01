eb23a1ecaed0b77c34b5743802543d9698714b06
IsolatedSushi <s.vanherpt@students.uu.nl>
2021-10-02 01:38:46 +0200

    Check the pima dataset with confusion matrix

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 from decision_tree import DescisionTree
 from visualiser import Visualiser
-
-DATAPATH = 'data/credit.txt'
+from sklearn.metrics import confusion_matrix
+DATAPATH = 'data/pimaIndians.txt'
 DELIMETER = ','
-SKIP_HEADER = True
+SKIP_HEADER = False
 
 def main():
     #Get the data, and split the values from the labels
@@ -13,17 +13,26 @@ def main():
     x = credit_data[:,:-1]
     y = credit_data[:,-1]
 
-    tree = tree_grow(x, y, 2, 1)
+    tree = tree_grow(x, y, 20, 5)
 
     #Visualise the tree in the console
-    visualiser = Visualiser()
-    visualiser.visualiseTree(tree)
+    #visualiser = Visualiser()
+    #visualiser.visualiseTree(tree)
+
+    predictions = tree_pred(x, tree)
+    print(confusion_matrix(y, predictions))
+
+
 
 #Construct and return the tree
 def tree_grow(x, y, nmin: int, minleaf: int): #, nfeat):
     tree = DescisionTree()
     tree.construct(x, y, nmin, minleaf)
     return tree
+
+def tree_pred(x, tr):
+    predictedLabels = tr.predict(x)
+    return predictedLabels
 
 if __name__ == "__main__":
     main()
