90a771b552557c7cf42541ae90471be7b324d153
Dimitar Angelov <dmapostbox@gmail.com>
2021-10-06 16:03:24 +0300

    Implemented use of Eclipse dataset and the 3 analyses

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 import numpy as np
-import pandas as pd
+# import pandas as pd
 from decision_tree import DescisionTree
 from visualiser import Visualiser
 from sklearn.metrics import confusion_matrix
-from statistics import mode
 from sklearn.metrics import accuracy_score
 from functools import wraps
 from time import time
 
-DATAPATH = 'data/pimaIndians.txt'
-DELIMETER = ','
-SKIP_HEADER = False
+DATAPATH = 'data/eclipse-metrics-packages-2.0.csv'
+DELIMETER = ';'
+SKIP_HEADER = True
 
 
 def main():
-    # Get the data, and split the values from the labels
-    credit_data = np.genfromtxt(DATAPATH, delimiter=DELIMETER, skip_header=SKIP_HEADER)
-    x = credit_data[:, :-1]
-    y = credit_data[:, -1]
+    # Get the training data, and split the values from the labels
+    eclipse_data_train = np.genfromtxt(DATAPATH, delimiter=DELIMETER, skip_header=SKIP_HEADER)
+    x_train = eclipse_data_train[:, 2:44]  # we want all of this slice other than 3
+    x_train = np.delete(x_train, 1, 1)  # can this and the above be done in 1 line?
 
-    test_pred_b(x, y, 20, 5, 4, 1)
-    test_pred_b(x, y, 20, 5, 4, 10)
-    test_pred_b(x, y, 20, 5, 4, 100)
+    y_train = eclipse_data_train[:, 3]
+    # we want y to be a boolean (we try to predict whether there is or is not a bug, not how many there are)
+    y_train[y_train > 0] = 1  # so we change all non-0 values to 1 https://stackoverflow.com/a/19666680/14598178
 
-    
+    # Get the test data
+    eclipse_data_test = np.genfromtxt('data/eclipse-metrics-packages-3.0.csv', delimiter=DELIMETER, skip_header=SKIP_HEADER)
+    x_test = eclipse_data_test[:, 2:44]  # we want all of this slice other than 3
+    x_test = np.delete(x_test, 1, 1)  # can this and the above be done in 1 line?
+
+    y_test = eclipse_data_test[:, 3]
+    # we want y to be a boolean (we try to predict whether there is or is not a bug, not how many there are)
+    y_test[y_test > 0] = 1  # so we change all non-0 values to 1 https://stackoverflow.com/a/19666680/14598178
+
+    test_pred_b(x_train, y_train, x_test, y_test, 15, 5, 41, 1)  # for Analysis 1
+    test_pred_b(x_train, y_train, x_test, y_test, 15, 5, 41, 100)  # for Analysis 2
+    test_pred_b(x_train, y_train, x_test, y_test, 20, 5, 6, 100)  # for Analysis 3
+
     # Visualise the tree in the console
     # visualiser = Visualiser()
     # visualiser.visualiseTree(tree)
+
 
 # Construct and return the tree
 def tree_grow(x, y, nmin: int, minleaf: int, nfeat: int):
@@ -52,16 +64,16 @@ def tree_pred_b(tree_list, x):
     allResults = [tree_pred(x, tree) for tree in tree_list]
     allResultsArray = np.array(allResults)
 
-    #Get the total votes for classifing 1
+    # Get the total votes for classifying 1
     summedResults = allResultsArray.sum(axis=0)  # get a sum of the results from each tree for each row
-    
-    #If the sum is bigger than half the dataset (rounded down for tiebreakers) then it is the majority
+
+    # If the sum is bigger than half the dataset (rounded down for tiebreakers) then it is the majority
     finalResult = summedResults > len(allResults) // 2
-    
+
     return finalResult
 
 
-#For debugging
+# For debugging
 def timing(f):
     @wraps(f)
     def wrap(*args, **kw):
@@ -69,17 +81,20 @@ def timing(f):
         result = f(*args, **kw)
         te = time()
         print('func:%r took: %2.4f sec' % \
-          (f.__name__, te-ts))
+              (f.__name__, te - ts))
         return result
+
     return wrap
 
+
 @timing
-def test_pred_b(x, y, nmin: int, minleaf: int, nfeat: int, m: int):
-    treeList = tree_grow_b(x, y, nmin, minleaf, nfeat, m)
-    baggingPredictions = tree_pred_b(treeList, x)
-    print("m: ",m)
-    print(accuracy_score(y,baggingPredictions))
-    print(confusion_matrix(y, baggingPredictions))
+def test_pred_b(x_train, y_train, x_test, y_test, nmin: int, minleaf: int, nfeat: int, m: int):
+    treeList = tree_grow_b(x_train, y_train, nmin, minleaf, nfeat, m)
+    baggingPredictions = tree_pred_b(treeList, x_test)
+    print("m: ", m)
+    print(accuracy_score(y_test, baggingPredictions))
+    print(confusion_matrix(y_test, baggingPredictions))
+
 
 if __name__ == "__main__":
     main()
