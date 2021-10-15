17893c1adfeeca19444f14d331a05ae665eb2a4f
Dimitar Angelov <dmapostbox@gmail.com>
2021-10-15 15:20:13 +0300

    Final refactoring to finalize the assignment. Everything was homogenized into one file. Old files and additional functions were removed. big changes

--- main.py
+++ main.py
@@ -1,15 +1,245 @@
 import numpy as np
-from typing import List, Any
-
-from decision_tree import DescisionTree
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-from functools import wraps
-from time import time
+from typing import List, Tuple, Union
 
 DATAPATH = 'data/eclipse-metrics-packages-2.0.csv'
 DELIMETER = ';'
 SKIP_HEADER = True
+
+
+class Node:
+    def __init__(self) -> None:
+        self.left = None
+        self.right = None
+        self.splitFunction = None
+        self.finalClassLabel = None
+
+        self.index = None
+        self.splitValue = None
+
+        # Debugging purposes
+        self.trainingValuesIndices = None
+        self.trainingLabels = None
+
+    # Set the childNodes
+    def setChildNodes(self, left, right) -> None:
+        self.left = left
+        self.right = right
+
+    # Set the splitValues
+    def setSplitValues(self, index, splitValue) -> None:
+        self.index = index
+        self.splitValue = splitValue
+
+    # Get the majority class label for this node
+    def setFinalClassLabel(self, labels) -> None:
+        self.trainingLabels = labels
+
+        positiveAmount = np.sum(labels)
+        negativeAmount = len(labels) - positiveAmount
+
+        # TODO figure out tie breaker
+        if positiveAmount > negativeAmount:
+            self.finalClassLabel = 1
+        else:
+            self.finalClassLabel = 0
+
+    # Make the prediction with a datarow
+    def predict(self, dataRow) -> None:
+        if self.index is None:  # Leaf node
+            return self.finalClassLabel
+
+        # Debugging purposes
+        assert self.left is not None
+        assert self.right is not None
+
+        # Iterate to the next node according to the split value
+        if dataRow[self.index] <= self.splitValue:
+            return self.left.predict(dataRow)
+        else:
+            return self.right.predict(dataRow)
+
+
+def gini_index(labels: np.ndarray) -> float:
+    """
+    Calculate the gini index as shown in the lecture slides
+
+    :param labels: np.ndarray
+    :return: Gini index (float)
+    """
+    totalTrue = np.sum(labels)  # Get the amount of true labels
+    probTrue = totalTrue / labels.shape[0]  # Calculate the proportion of true labels
+
+    return probTrue * (1 - probTrue)
+
+
+class DecisionTree:
+    def __init__(self) -> None:
+        self.rootNode = None
+        self.nmin = None
+        self.minleaf = None
+        self.nfeat = None
+
+    def predict(self, dataRows: np.ndarray) -> list:
+        """
+        Make the predictions
+
+        :param dataRows: rows of the data to make predictions on
+        :return: list of predictions
+        """
+        resultLabels = [self.rootNode.predict(row) for row in dataRows]
+        return resultLabels
+
+    def construct(self, x: np.ndarray, y: np.ndarray, nmin: int, minleaf: int, nfeat: int) -> None:
+        """
+        Construct the trees
+
+        :param x: array of features and their values
+        :param y: labels
+        :param nmin: minimum number of observations that a node must contain, for it to be allowed to be split
+        :param minleaf: minimum number of observations required for a leaf node
+        :param nfeat: the number of features that should be considered for each split
+        :return: None
+        """
+        # Store the parameters
+        self.nmin = nmin
+        self.minleaf = minleaf
+        self.nfeat = nfeat
+
+        # Check whether there is any training data, and whether each data point has a label
+        assert x.shape > (0, 0)
+        assert x.shape[0] == y.shape[0]
+        # Check that nfeat is more than 0 and isn't a number greater than the number of columns in the data
+        assert 0 < nfeat <= x.shape[1]
+
+        self.rootNode = self.grow_tree(x, y)
+
+    def getPossibleSplits(self, x) -> List[Tuple[int, float]]:
+        """
+        Get all the possible splits, for all the features
+
+        :param x: array of features and their values
+        :return: combinations of possible splits
+        """
+        # Choose random indices (columns of x), of size nfeat, without replacement
+        # We use this to choose the nfeat features we are interested in
+        featureIndices = np.random.choice(range(x.shape[1]), size=self.nfeat, replace=False)
+        featureIndices.sort()
+
+        # We want a list of possible splits which contains the values of the splits for features which we are interested
+        # in and None values for the features we are not interested in
+        possibleSplits = np.full(x.shape[1], None, dtype=object)
+        possibleSplits[featureIndices] = [self.getSplitsPerFeature(x, index) for index in featureIndices]
+
+        # Store it in a tuple for easy access TODO add explanation
+        allCombinations = [(index, splitValue) for index in featureIndices for splitValue in possibleSplits[index]]
+        return allCombinations
+
+    def getSplitsPerFeature(self, x: np.ndarray, featureIndex: np.int32) -> float:
+        """
+        Get all possible splits in a single feature
+        (halfway between consecutive values because the data is all numerical)
+
+        :param x: array of features and their values
+        :param featureIndex: array of indices of features to consider
+        :return: featureValuesAveraged
+        """
+        featureValuesSorted = np.sort(np.unique(x[:, featureIndex]))
+        featureValuesAveraged = (featureValuesSorted[0:-1] + featureValuesSorted[1:]) / 2
+
+        return featureValuesAveraged
+
+    def getBestSplit(self, x: np.ndarray, y: np.ndarray, allCombinations: list) -> Union[Tuple[int, float], None]:
+        """
+        :param x: array of features and their values
+        :param y: labels
+        :param allCombinations: list of combinations of possible splits
+        :return: the best splitCombination according to the impurity function
+        """
+        allScores = [self.GetCurrentScore(x, y, combination) for combination in allCombinations]
+        bestCombination = allCombinations[np.argmin(allScores)]
+
+        if np.min(allScores) == 1000:  # Since the reduction can't be more than 1 anyway,
+            # this is a sufficient check on whether the split is allowed
+            return None
+        return bestCombination
+
+    def GetCurrentScore(self, x, y, combination) -> Union[float, int]:
+        """
+        Returns the delta i, without calculating the impurity of current node (is constant),
+        so you want the smallest one
+
+        :param x: array of features and their values
+        :param y: labels
+        :param combination: split combination
+        :return: delta i
+        """
+        # TODO probably more efficient to not make the split itself, but use the method as described
+        #  in the assignment get started
+        xLeft, xRight, yLeft, yRight = self.getCurrentSplit(x, y, combination)
+
+        # Check the minleaf restriction
+        if len(yLeft) < self.minleaf or len(yRight) < self.minleaf:
+            return 1000  # Since the reduction can't be more than 1 anyway, 1000 is more than sufficient.
+
+        return gini_index(yLeft) * len(yLeft) / len(y) + gini_index(yRight) * len(yRight) / len(y)
+
+    def getCurrentSplit(self, x: np.ndarray, y: np.ndarray, combination: Tuple[int, float]) -> \
+            Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """
+        :param x: array of features and their values
+        :param y: labels
+        :param combination: split combination
+        :return: split dataset and labels according to combination
+        """
+        featureIndex, splitValue = combination
+        # Get the masks for easy access
+        leftMask = x[:, featureIndex] < splitValue
+        rightMask = ~leftMask
+
+        return x[leftMask], x[rightMask], y[leftMask], y[rightMask]
+
+    def grow_tree(self, x: np.ndarray, y: np.ndarray) -> Node:
+        """
+        Grow the tree (recursive function)
+
+        :param x: array of features and their values
+        :param y: labels
+        :return: currentNode
+        """
+        currentNode = Node()
+        currentNode.setFinalClassLabel(y)  # Get the majority vote for each Node
+
+        currentNode.trainingValuesIndices = x[:, 0]  # Debugging values
+
+        if gini_index(y) == 0:  # All labels are the same
+            return currentNode
+
+        if len(y) < self.nmin:  # The restriction according to the assignment
+            return currentNode
+
+        allCombinations = self.getPossibleSplits(x)
+        if len(allCombinations) == 0:  # No more possible combinations (needed if nmin = 0)
+            return currentNode
+
+        # Get the best split and split the data accordingly
+        bestSplit = self.getBestSplit(x, y, allCombinations)
+
+        # Check if there is an allowed split
+        if bestSplit is None:
+            return currentNode
+
+        bestFeatureIndex, bestSplitValue = bestSplit
+        xLeft, xRight, yLeft, yRight = self.getCurrentSplit(x, y, (bestFeatureIndex, bestSplitValue))
+        # Since it only happens once every iteration and is written in C no need to cache the results earlier.
+        # Redoing it is fine since it improves clean code
+
+        # recursively generate the child nodes as well
+        currentNode.left = self.grow_tree(xLeft, yLeft)
+        currentNode.right = self.grow_tree(xRight, yRight)
+
+        currentNode.setSplitValues(bestFeatureIndex, bestSplitValue)
+
+        return currentNode
 
 
 def main():
@@ -32,30 +262,13 @@ def main():
     # we want y to be a boolean (we try to predict whether there is or is not a bug, not how many there are)
     y_test[y_test > 0] = 1  # so we change all non-0 values to 1
 
-    get_training_data(x_train, y_train, x_test, y_test)
-
-    # Visualise the tree in the console
-    # visualiser = Visualiser()
-    # visualiser.visualiseTree(tree)
-
-
-def get_training_data(x_train, y_train, x_test, y_test):
-    for i in range(100):
-        t1 = time()
-        test_pred_b(x_train, y_train, x_test, y_test, 15, 5, 6, 100)  # for Analysis 3
-        print("Iteration", i, " took", time() - t1, " seconds")
-
-    print("accuracies", accuracies)
-    print("precisions", precisions)
-    print("recalls", recalls)
-    print("confusion matrices", confusion_matrices)
-    # test_pred(x_train, y_train, x_test, y_test, 15, 5, 41)  # for Analysis 1
-    # test_pred_b(x_train, y_train, x_test, y_test, 15, 5, 41, 100)  # for Analysis 2
-    # test_pred_b(x_train, y_train, x_test, y_test, 15, 5, 6, 100)  # for Analysis 3
+    # Tree_1 = tree_grow(x_train, y_train, 15, 5, 41)  # analysis 1
+    # Tree_2 = tree_grow_b(x_train, y_train, 15, 5, 41, 100)  # analysis 2
+    # Tree_3 = tree_grow_b(x_train, y_train, 15, 5, 6, 100)  # analysis 3
 
 
 # Construct and return the tree
-def tree_grow(x: np.ndarray, y: np.ndarray, nmin: int, minleaf: int, nfeat: int) -> DescisionTree:
+def tree_grow(x: np.ndarray, y: np.ndarray, nmin: int, minleaf: int, nfeat: int) -> DecisionTree:
     """
     Construct the tree
 
@@ -66,12 +279,12 @@ def tree_grow(x: np.ndarray, y: np.ndarray, nmin: int, minleaf: int, nfeat: int)
     :param nfeat: the number of features that should be considered for each split
     :return: Trained decision tree
     """
-    tree = DescisionTree()
+    tree = DecisionTree()
     tree.construct(x, y, nmin, minleaf, nfeat)
     return tree
 
 
-def tree_pred(x: np.ndarray, tr: DescisionTree) -> list:
+def tree_pred(x: np.ndarray, tr: DecisionTree) -> list:
     """
     Make predictions with a tree
 
@@ -84,7 +297,7 @@ def tree_pred(x: np.ndarray, tr: DescisionTree) -> list:
 
 
 # Random forests
-def tree_grow_b(x: np.ndarray, y: np.ndarray, nmin: int, minleaf: int, nfeat: int, m: int) -> List[DescisionTree]:
+def tree_grow_b(x: np.ndarray, y: np.ndarray, nmin: int, minleaf: int, nfeat: int, m: int) -> List[DecisionTree]:
     """
     Construct m number of trees (for bagging/rf)
 
@@ -123,45 +336,6 @@ def tree_pred_b(tree_list: List, x: np.ndarray) -> bool:
     finalResult = summedResults > len(allResults) // 2
 
     return finalResult
-
-
-# For debugging
-def timing(f: Any) -> Any:
-    @wraps(f)
-    def wrap(*args, **kw) -> Any:
-        ts = time()
-        result = f(*args, **kw)
-        te = time()
-        print('func:%r took: %2.4f sec' % \
-              (f.__name__, te - ts))
-        return result
-
-    return wrap
-
-
-accuracies = []
-precisions = []
-recalls = []
-confusion_matrices = []
-
-
-def test_pred(x_train, y_train, x_test, y_test, nmin: int, minleaf: int, nfeat: int) -> None:
-    tree = tree_grow(x_train, y_train, nmin, minleaf, nfeat)
-    treePrediction = tree_pred(x_test, tree)
-    accuracies.append(accuracy_score(y_test, treePrediction))
-    precisions.append(precision_score(y_test, treePrediction))
-    recalls.append(recall_score(y_test, treePrediction))
-    confusion_matrices.append(confusion_matrix(y_test, treePrediction).tolist())
-
-
-def test_pred_b(x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_test: np.ndarray, nmin: int,
-                minleaf: int, nfeat: int, m: int) -> None:
-    treeList = tree_grow_b(x_train, y_train, nmin, minleaf, nfeat, m)
-    baggingPredictions = tree_pred_b(treeList, x_test)
-    accuracies.append(accuracy_score(y_test, baggingPredictions))
-    precisions.append(precision_score(y_test, baggingPredictions))
-    recalls.append(recall_score(y_test, baggingPredictions))
-    confusion_matrices.append(confusion_matrix(y_test, baggingPredictions).tolist())
 
 
 if __name__ == "__main__":
